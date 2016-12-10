43a3897f0310dc4ced5466973fdf2fe02703315b
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2016-12-10 21:03:31 +1100

    Initial commit

---
+++ front-end/Entry.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Welcome = () => (
+	<div>
+		Welcome to Sooriyavilla
+	</div>
+)
+
+ReactDOM.render(<Welcome/>, document.getElementById("app"))
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+
+module.exports = {
+	entry: path.join(__dirname, "front-end", "Entry.jsx"),
+	output: {
+		path: path.join(__dirname, "public", "build"),
+		publicPath: "/build/",
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /(.js|.jsx)$/,
+				loader: "babel"
+			},
+			{
+				test: /\.scss$/,
+				loaders: ["style", "css", "sass"]
+			},
+			{
+				test: /(.jpe?g|.svg|.png|.gif)$/,
+				loader: "file?name=[path][name].[ext]"
+			}
+		]
+	}
+}
